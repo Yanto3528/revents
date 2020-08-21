afcdf077fe44e20237baf4054af417006c3b0d86
Yanto Lee <yanto3528@gmail.com>
2020-08-21 11:30:34 +0700

    adding event dashboard, event list, and event form component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
+import "semantic-ui-css/semantic.min.css";
 import "./app/layout/styles.css";
 
 const rootEl = document.getElementById("root");
